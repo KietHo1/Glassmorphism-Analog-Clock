c2c686ef9d1aadc7da09dc61f0f123150d4710cd
KietHo1 <kiethostartup1999@gmail.com>
2022-10-13 14:54:16 +0700

    Edit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ function App() {
           <title>Code With Hossein</title>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-          <link rel="stylesheet" href="../css/style.css" />
         </head>
 
         <body>
