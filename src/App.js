48fe1172ff40d062bfef321eff4b047ffb499366
KietHo1 <kiethostartup1999@gmail.com>
2022-10-13 14:13:06 +0700

    Edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <head>
+          <title>Code With Hossein</title>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <link rel="stylesheet" href="../css/style.css" />
+        </head>
+
+        <body>
+          <div class="container">
+            <div class="circle"></div>
+            <div class="circle"></div>
+
+            <div class="clock">
+              <div class="clock-bg">
+                <img src="../images/clock.png" alt="..." />
+              </div>
+              <div class="hour">
+                <span class="hr" id="hr"></span>
+              </div>
+              <div class="minute">
+                <span class="min" id="min"></span>
+              </div>
+              <div class="second">
+                <span class="sec" id="sec"></span>
+              </div>
+            </div>
+          </div>
+
+          <script type="text/javascript" src="../js/script.js"></script>
+        </body>
+      </div>
     </div>
   );
 }
